5bcb03a2c09f1219e0f9ff8474ebf7209a86aae0
Danya76131 <danylo.galetskiy@gmail.com>
2025-05-29 15:31:13 +0200

    fix L

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,13 +3,11 @@ import styles from "./FriendListItem.module.css";
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={styles.item}>
-      <span
-        className={`${styles.status} ${
-          isOnline ? styles.online : styles.offline
-        }`}
-      />
       <img className={styles.avatar} src={avatar} alt="Avatar" />
       <p className={styles.name}>{name}</p>
+      <span className={` ${isOnline ? styles.online : styles.offline}`}>
+        {isOnline ? "Online" : "Offline"}
+      </span>
     </li>
   );
 };
